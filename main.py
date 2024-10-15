41a0c788aa0df2f61c790e6137b143aa5c3d27b3
VitorCamposAds <vitorbeatle@gmail.com>
2024-10-15 02:20:47 -0300

    criação da tabela

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from fakepinterest import app
+from fakepinterest import app, database
 
 if __name__ == "__main__":
+    with app.app_context():
+        database.create_all()  # Cria as tabelas no banco de dados
     app.run(debug=False)
+
